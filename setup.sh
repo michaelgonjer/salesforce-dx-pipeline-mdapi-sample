7ace93838e4e2f9115184628f1688e7e77f0a224
Juan Tadeo Chilavert Espinola <j.gilabert.espinola@acccenture.com>
2018-06-20 13:17:04 +0200

    actualizacion del repositorio

--- setup.sh
+++ setup.sh
@@ -14,7 +14,7 @@ set -o nounset    # fail on unset variables
 TICKS=$(echo $(date +%s | cut -b1-13))
 
 # Name of your team (optional)
-HEROKU_TEAM_NAME="appcloud-dev" 
+HEROKU_TEAM_NAME="" 
 
 # Name of the Heroku apps you'll use
 HEROKU_DEV_APP_NAME="dev$TICKS"
@@ -25,10 +25,10 @@ HEROKU_PROD_APP_NAME="prod$TICKS"
 HEROKU_PIPELINE_NAME="pipeline$TICKS"
 
 # Usernames or aliases of the orgs you're using
-DEV_HUB_USERNAME="HubOrg"
-DEV_USERNAME="DevOrg"
-STAGING_USERNAME="TestOrg"
-PROD_USERNAME="ProdOrg"
+DEV_HUB_USERNAME="j.gilabert@sfdxdemo.com"
+DEV_USERNAME="j.gilabert@sfdxdemo.com"
+STAGING_USERNAME="j.gilabert@sfdxdemo.com"
+PROD_USERNAME="j.gilabert@sfdxdemo.com"
 
 # Repository with your code
 GITHUB_REPO="wadewegner/salesforce-dx-pipeline-sample"
